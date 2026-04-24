913c173c3c13826565276978afacd20df5c0e685
SnackeyesD <messiasayi@gmail.com>
2026-04-25 00:45:06 +0100

    final build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
-    allowedHosts: ['.trycloudflare.com']
+    allowedHosts: [".trycloudflare.com"],
   },
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+});
